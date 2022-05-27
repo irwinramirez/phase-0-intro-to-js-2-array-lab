1b14b750dae318755540817ad8c88810f2fc6d86
Irwin Ramirez <Irwinramirez23@gmail.com>
2022-05-26 21:22:15 -0400

    commit tree 9c7e8cde7b15c142968f654619d87be60d22f19f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(){
+    cats.push("Ralph")
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+    let moreCats = [...cats, "Broom"]
+    return moreCats
+}
+function prependCat(){
+    let evenMoreCats = ["Arnold", ...cats]
+    return evenMoreCats
+}
+function removeLastCat(){
+    let lessCats = [...cats.slice(0, -1)]
+    return lessCats
+}
+
+function removeFirstCat(){
+    let evenLessCats = [...cats.slice(1,3)]
+    return evenLessCats
+}
